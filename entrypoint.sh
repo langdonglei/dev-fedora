fe0b9f07f781bfe8c445ae4d954a47a20168461f
langdonglei <langdonglei@icloud.com>
2025-06-15 23:10:43 +0800

    refactor(entrypoint): 移除 PHP 7.3 版本支持
    - 删除了用于启动 PHP 7.3 版本 php-fpm 的代码块
    -保留了 PHP 7.4 和 PHP 8.0 版本的支持
    - 优化了代码结构，提高了可读性和维护性

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,6 @@
 nginx
 
 # 启动 php-fpm
-if test $1 == 73 ; then
-  /opt/remi/php73/root/sbin/php-fpm -R
-fi
-
 if test $1 == 74 ; then
   /opt/remi/php74/root/sbin/php-fpm -R
 fi
@@ -13,7 +9,6 @@ fi
 if test $1 == 80 ; then
   /opt/remi/php80/root/sbin/php-fpm -R
 fi
-
 
 # 启动 redis
 redis-server /etc/redis/redis.conf
