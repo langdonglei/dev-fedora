e25bcc47cc7c0d7943194fbbdea693ff8d67ea17
langdonglei <langdonglei@icloud.com>
2025-06-15 23:00:56 +0800

    feat(entrypoint): 支持多版本 PHP
    - 添加对 PHP7.3、7.4 和 8.0 的支持
    - 根据传入参数启动对应版本的 PHP-FPM
    - 更新 Remi 仓库版本从 42 到 40

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,18 @@
 nginx
 
 # 启动 php-fpm
-php-fpm -D -R
+if test $1 == 73 ; then
+  /opt/remi/php73/root/sbin/php-fpm -R
+fi
+
+if test $1 == 74 ; then
+  /opt/remi/php74/root/sbin/php-fpm -R
+fi
+
+if test $1 == 80 ; then
+  /opt/remi/php80/root/sbin/php-fpm -R
+fi
+
 
 # 启动 redis
 redis-server /etc/redis/redis.conf
--- install.sh
+++ install.sh
@@ -64,7 +64,7 @@ echo 'bind * -::*' >> /etc/redis/redis.conf
 echo 'protected-mode no' >> /etc/redis/redis.conf
 
 # php
-dnf install -y https://rpms.remirepo.net/fedora/remi-release-42.rpm
+dnf install -y https://rpms.remirepo.net/fedora/remi-release-40.rpm
 for i in 73 74 80
 do
     dnf install -y \
